6a9b89651ba9db599fd625a95defba70f0c45c66
vikashvk <vikash.a.vikash@capgemini.com>
2018-11-13 15:00:04 +0530

    hr

---
+++ MavenExample/src/test/java/com/capgemini/app/AppTest.java
@@ -0,0 +1,23 @@
+package com.capgemini.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+    /**
+     * Create the test case
+     */
+
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
